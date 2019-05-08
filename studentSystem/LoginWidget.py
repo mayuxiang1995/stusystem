4e184f52f4772945c07ec9b4c7c110610c63a3c2
mayuxiang <207658567@qq.com>
2019-05-08 10:34:41 +0800

    登陆界面的搭建

---
+++ studentSystem/LoginWidget.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import *
+from PyQt5.Qt import QFont
+
+
+class LoginWidget(QWidget):
+
+    def __init__(self):
+        super(LoginWidget, self).__init__()
+
+        # 首先定义一个水平布局
+        hbox = QHBoxLayout(self)
+        hbox.addStretch()
+        # 定义一个垂直布局
+        vbox = QVBoxLayout()
+        hbox.addLayout(vbox)
+        hbox.addStretch()
+        q_label = QLabel('学生管理系统')
+        font = QFont("微软雅黑", 30)
+        font.setBold(True)
+        q_label.setFont(font)
+        vbox.addWidget(q_label)
+        # 定义表单布局
+        formlayout = QFormLayout()
+        vbox.addLayout(formlayout)
+        # 添加用户名:为什么用self，因为要用到次输入框
+        self.user_nameEdit = QLineEdit()
+        formlayout.addRow("用户名", self.user_nameEdit)
+        self.user_passwordEdit = QLineEdit()
+        self.user_passwordEdit.setEchoMode(QLineEdit.Password)
+        formlayout.addRow("密码", self.user_passwordEdit)
+        # 添加注册按钮
+        button = QPushButton("登陆")
+        formlayout.addRow("", button)
