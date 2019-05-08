e9604fea2cdbfded8378aba7d79831aeab55055e
mayuxiang <207658567@qq.com>
2019-05-08 15:44:07 +0800

    Qt操作数据库

--- studentSystem/InformationWidget.py
+++ studentSystem/InformationWidget.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtSql import *
+from PyQt5.QtCore import Qt
 
 
 class InformationWidget(QWidget):
@@ -24,22 +26,51 @@ class InformationWidget(QWidget):
         removebutton.clicked.connect(self.remove)
         vbox.addWidget(removebutton)
         exitbutton = QPushButton("退出")
-        exitbutton.clicked.connect(self.exit)
+        exitbutton.clicked.connect(QApplication.exit)
         vbox.addWidget(exitbutton)
 
         hbox.addWidget(groupbox)
+
         # 定义右侧的tableview
-        tableview = QTableView()
-        hbox.addWidget(tableview)
+        self.tableview = QTableView()
+        hbox.addWidget(self.tableview)
+
+        # 创建db
+        db = QSqlDatabase.addDatabase("QMYSQL")
+        # 设置参数
+        db.setHostName('localhost')
+        db.setPort(3306)
+        db.setUserName('stu')
+        db.setPassword('123456')
+        db.setDatabaseName('stusystem')
+        # 打开连接
+        db.open()
+        # tabelemodel封装了增删改查的操作
+        self.tableModel = QSqlTableModel(db=db)
+        self.tableModel.setTable('student')
+        # 设置表头
+        self.tableModel.setHeaderData(0, Qt.Horizontal, '学号')
+        self.tableModel.setHeaderData(1, Qt.Horizontal, '姓名')
+        self.tableModel.setHeaderData(2, Qt.Horizontal, '年龄')
+        self.tableModel.setHeaderData(3, Qt.Horizontal, '性别')
+
+        # 将tablemodel与tableview进行绑定
+        self.tableview.setModel(self.tableModel)
 
     def query(self):
-        pass
+        print("查看所有数据")
+        self.tableModel.select()
 
     def add(self):
-        pass
+        print("添加一行")
+        # 获取出一共有多少行
+        count = self.tableModel.rowCount()
+        # 需要在哪一行插入
+        self.tableModel.insertRow(count)
 
     def remove(self):
-        pass
+        print("删除一行")
+        # 找到用户选中的行
+        index = self.tableview.currentIndex().row()
+        self.tableModel.removeRow(index)
 
-    def exit(self):
-        pass
